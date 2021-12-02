9e494b0152673cc7b41b2cb5367ed940c0b6dedf
刘启明 <455043818@qq.com>
2021-12-02 20:22:01 +0800

    feat: 拉起手机号获取，待解密

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,24 +1,56 @@
 <template>
-  <div class="title">222</div>
+  <button
+    type="primary"
+    @click="clickPhoneNumber"
+    open-type="getPhoneNumber"
+    @getphonenumber="getPhoneNumber"
+    >获取手机号</button
+  >
 </template>
 
 <script setup>
-  import { reactive } from 'vue'
-  // import swiperComVue  from "../../components/swiperCom.vue";
-  const state = reactive({
-    msg: '欢迎使用 NutUI3.0 开发小程序',
-    msg2: '你成功了～',
-    type: 'text',
-    show: false,
-    cover: false
-  })
+  import { reactive, onMounted } from 'vue'
+  import Taro from '@tarojs/taro'
 
-  const handleClick = (type, msg, cover = false) => {
-    state.show = true
-    state.msg2 = msg
-    state.type = type
-    state.cover = cover
+  const clickPhoneNumber = () => {
+    console.log('clickPhoneNumber')
   }
+  const getPhoneNumber = (e) => {
+    console.log('ee', 'bb')
+    console.log(e.detail.errMsg)
+    console.log(e.detail.iv)
+    console.log(e.detail.encryptedData)
+  }
+
+  onMounted(() => {
+    console.log('ssss--onMounted')
+
+    Taro.checkSession({
+      success() {
+        // session_key 未过期，并且在本生命周期一直有效
+        console.log('还有效，无需获取')
+      },
+      fail() {
+        // session_key 已经失效，需要重新执行登录流程
+        console.log('已失效，要重新登录')
+        Taro.login({
+          success(res) {
+            if (res.code) {
+              // 发起网络请求
+              // Taro.request({
+              //   // url: 'https://test.com/onLogin',
+              //   // data: {
+              //   //   code: res.code
+              //   // }
+              // })
+            } else {
+              console.log(`登录失败！${res.errMsg}`)
+            }
+          }
+        })
+      }
+    })
+  })
 </script>
 
 <style lang="scss">
