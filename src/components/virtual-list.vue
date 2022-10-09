b2663c1de84a0779729786748f224f30b15828a2
陈双伟 <kerwin.chen@sunlight-tech.com>
2022-10-09 14:43:45 +0800

    fix: 滚动列表使用nut-infiniteloading（h5无法滚动）

--- src/components/virtual-list.vue
+++ src/components/virtual-list.vue
@@ -1,82 +1,82 @@
 <template>
-  <view class="container aaaa">
-    <view class="page-body">
-      <view class="page-section">
-        <!-- <view class="page-section-title">
-          <text>Vertical Scroll - 纵向滚动</text>
-        </view> -->
-        <view class="page-section-spacing">
-          <scroll-view
-            :scroll-y="true"
-            style="height: 300px"
-            @scrolltoupper="upper"
-            @scrolltolower="lower"
-            @scroll="scroll"
-            :lowerThreshold="200"
-            :scroll-into-view="state.toView"
-            :scroll-top="state.scrollTop"
-          >
-            <view id="demo1" class="scroll-view-item demo-text-1">1</view>
-            <view id="demo2" class="scroll-view-item demo-text-2">2</view>
-            <view id="demo3" class="scroll-view-item demo-text-3">3</view>
-          </scroll-view>
-        </view>
-      </view>
-    </view>
-  </view>
+  <ul class="infiniteUl" id="refreshScroll" style="height: 100vh">
+    <!-- container-id="refreshScroll" -->
+    <nut-infiniteloading
+      pull-icon="JD"
+      :use-window="true"
+      container-id="refreshScroll"
+      :is-open-refresh="true"
+      :has-more="refreshHasMore"
+      @load-more="refreshLoadMore"
+      :use-capture="true"
+      @refresh="refresh"
+      @scroll-change="scrollChange"
+    >
+      <li class="infiniteLi" v-for="(item, index) in refreshList" :key="index">{{ item }}</li>
+    </nut-infiniteloading>
+  </ul>
 </template>
 
-<script setup>
-  import { reactive } from 'vue'
+<script>
+  import { ref, reactive, onMounted, toRefs } from 'vue'
 
-  const state = reactive({
-    scrollTop: 0,
-    toView: 'demo2'
-  })
+  export default {
+    setup() {
+      const refreshHasMore = ref(true)
+      const data = reactive({
+        refreshList: []
+      })
+      const refreshLoadMore = (done) => {
+        console.log('独自守着伤悲')
+        setTimeout(() => {
+          const curLen = data.refreshList.length
+          for (let i = curLen; i < curLen + 10; i++) {
+            data.refreshList.push(`${i}`)
+          }
+          if (data.refreshList.length > 30) refreshHasMore.value = false
+          done()
+        }, 500)
+      }
 
-  const upper = (e) => {
-    console.log('upper:', e)
-  }
+      const refresh = (done) => {
+        setTimeout(() => {
+          // Toast.success('刷新成功')
+          done()
+        }, 1000)
+      }
+      const init = () => {
+        for (let i = 0; i < 9; i++) {
+          data.refreshList.push(`${i}`)
+        }
+      }
 
-  const lower = (e) => {
-    console.log('lower:', e)
-  }
-
-  const scroll = (e) => {
-    // console.log('scroll:', e)
+      const scrollChange = (e) => {
+        console.log('高度监听', e)
+      }
+      onMounted(() => {
+        init()
+      })
+      return { refreshLoadMore, refreshHasMore, scrollChange, refresh, ...toRefs(data) }
+    }
   }
 </script>
 
 <style>
-  /* { */
-  /* height: 300px !important;
-  } */
-  :deep(.hydrated),
-  :deep(.container) {
-    height: 300px !important;
-  }
-  .page-section-spacing {
-    margin-top: 60rpx;
-  }
-  .scroll-view_H {
-    white-space: nowrap;
-  }
-  .scroll-view-item {
-    height: 300rpx;
-  }
-  .scroll-view-item_H {
-    display: inline-block;
+  .infiniteUl {
+    height: 100vh;
     width: 100%;
-    height: 300rpx;
+    padding: 0;
+    margin: 0;
+    overflow-y: auto;
+    overflow-x: hidden;
+    background: #eee;
   }
-
-  .demo-text-1 {
-    background: #ccc;
-  }
-  .demo-text-2 {
-    background: #999;
-  }
-  .demo-text-3 {
-    background: #666;
+  .infiniteLi {
+    margin-top: 10px;
+    font-size: 14px;
+    color: rgba(100, 100, 100, 1);
+    text-align: center;
+    height: 80px;
+    background-color: #fff;
   }
 </style>
