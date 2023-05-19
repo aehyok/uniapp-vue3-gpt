10589f240058e0dc73938f82a1bbec8a53c54e23
陈双伟 <csw6624@163.com>
2023-05-19 16:48:45 +0800

    feat：首页和我的

---
+++ src/pages/home/index.vue
@@ -0,0 +1,129 @@
+<template>
+  <view class="container">
+    <view class="uni-margin-wrap">
+      <swiper
+        class="swiper"
+        circular
+        :indicator-dots="indicatorDots"
+        :autoplay="autoplay"
+        :interval="interval"
+        :duration="duration"
+      >
+        <swiper-item>
+          <view class="swiper-item uni-bg-red">
+            <image
+              src="https://img1.baidu.com/it/u=2463514011,1142503686&fm=253&fmt=auto&app=138&f=JPEG?w=888&h=500"
+            ></image>
+          </view>
+        </swiper-item>
+        <swiper-item>
+          <view class="swiper-item uni-bg-green">
+            <image
+              src="https://img2.baidu.com/it/u=923478948,106462584&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1684602000&t=7831fa62e948620a9b5fe4bfd68f14ab"
+            ></image>
+          </view>
+        </swiper-item>
+        <swiper-item>
+          <view class="swiper-item uni-bg-blue">
+            <image
+              src="https://img2.baidu.com/it/u=3107178066,1951512797&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1684602000&t=96d7f2157a399154b0ec24f0327d2e35"
+            ></image>
+          </view>
+        </swiper-item>
+      </swiper>
+    </view>
+
+    <uni-notice-bar
+      show-icon
+      scrollable
+      :speed="50"
+      text="人生是由各种不同的变故、循环不已的痛苦和欢乐组成的。那种永远不变的蓝天只存在于心灵中，向现实的人生去要求未免是奢望。"
+    />
+    <view class="issue">
+      <uni-easyinput class="textarea" type="textarea" v-model="value" placeholder="请输入您的内容"></uni-easyinput>
+      <button @click="editRoster">获取答案</button>
+    </view>
+    <uni-section class="mb-10" title="智慧广场" type="line"></uni-section>
+    <uni-card title="求智慧的天使" sub-title="性别：未知" extra="查看详情" :thumbnail="avatar" @click="onClick">
+      <text class="uni-body">少年人用什么洁净他的行为呢？</text>
+    </uni-card>
+    <uni-card title="访客" sub-title="性别：未知" extra="查看详情" :thumbnail="avatar" @click="onClick">
+      <text class="uni-body">什么是极乐世界？人真的有来生吗？。</text>
+    </uni-card>
+  </view>
+</template>
+
+<script setup>
+let value = "";
+const indicatorDots = true;
+const autoplay = true;
+const interval = 2000;
+const duration = 800;
+let radio1 = 0;
+const sex = [
+  {
+    text: "男",
+    value: 0,
+  },
+  {
+    text: "男",
+    value: 1,
+  },
+  {
+    text: "男",
+    value: 2,
+  },
+];
+const editRoster = () => {
+  uni.navigateTo({
+    url: "/ffp-pages/roster/roster-detail",
+  });
+};
+</script>
+<style lang="scss" scoped>
+.uni-margin-wrap {
+  width: 690rpx;
+  width: 100%;
+}
+.swiper {
+  height: 300rpx;
+}
+.swiper-item {
+  display: block;
+  height: 300rpx;
+  line-height: 300rpx;
+  text-align: center;
+  image {
+    width: 100%;
+    height: 100%;
+  }
+}
+.swiper-list {
+  margin-top: 40rpx;
+  margin-bottom: 0;
+}
+.uni-common-mt {
+  margin-top: 60rpx;
+  position: relative;
+}
+.info {
+  position: absolute;
+  right: 20rpx;
+}
+.uni-padding-wrap {
+  width: 550rpx;
+  padding: 0 100rpx;
+}
+.issue {
+  padding: 20rpx;
+  :deep(.uni-easyinput) {
+    border: 2rpx solid #fbe09d;
+  }
+  button {
+    margin-top: 10px;
+    background-color: #fbe09d;
+    color: #ff8158;
+    font-weight: 500;
+  }
+}
+</style>
---
+++ src/pages/mine/index.vue
@@ -0,0 +1,101 @@
+<template>
+  <view class="container">
+    <view class="header"> 登录 </view>
+    <view class="balance">
+      <view class="mybalance">我的余额</view>
+
+      <view class="card">
+        <uni-card title="剩余条数" extra="0" :thumbnail="avatar" @click="onClick">
+          <text class="uni-body">通过在线充值可以获得永久有效的余额</text>
+          <text class="gojunp">去充值></text>
+        </uni-card>
+        <uni-card title="体验条数" extra="0" :thumbnail="avatar" @click="onClick">
+          <text class="uni-body">通过在线充值可以获得永久有效的余额</text>
+          <text class="gojunp">做任务></text>
+        </uni-card>
+      </view>
+    </view>
+    <view class="sectioncontent">
+      <uni-section class="mb-10" title="余额明细" type="line">
+        <template v-slot:right> <uni-icons type="forward" size="15"></uni-icons> </template>
+      </uni-section>
+      <uni-section class="mb-10" title="我的点赞" type="line">
+        <template v-slot:right><uni-icons type="forward" size="15"></uni-icons> </template>
+      </uni-section>
+      <uni-section class="mb-10" title="我的收藏" type="line">
+        <template v-slot:right> <uni-icons type="forward" size="15"></uni-icons> </template>
+      </uni-section>
+      <uni-section class="mb-10" title="提问记录" type="line">
+        <template v-slot:right> <uni-icons type="forward" size="15"></uni-icons> </template>
+      </uni-section>
+      <uni-section class="mb-10" title="意见反馈" type="line">
+        <template v-slot:right> <uni-icons type="forward" size="15"></uni-icons> </template>
+      </uni-section>
+    </view>
+  </view>
+</template>
+
+<script setup>
+// const aaa =  ()  => {}
+let radio1 = 0;
+const sex = [
+  {
+    text: "男",
+    value: 0,
+  },
+  {
+    text: "男",
+    value: 1,
+  },
+  {
+    text: "男",
+    value: 2,
+  },
+];
+const editRoster = () => {
+  uni.navigateTo({
+    url: "/ffp-pages/roster/roster-detail",
+  });
+};
+</script>
+<style lang="scss" scoped>
+.header {
+  height: 300rpx;
+  line-height: 300rpx;
+  text-align: center;
+  background-color: #f1ece3;
+}
+.balance {
+  width: 95%;
+  box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.5);
+  border-radius: 6rpx;
+  margin: 20rpx auto;
+  box-sizing: border-box;
+  background-color: #fffaf6;
+}
+.mybalance {
+  color: #baa06c;
+  padding: 20rpx 0 0 30rpx;
+}
+.card {
+  display: flex;
+  justify-content: space-between;
+}
+.gojunp {
+  display: block;
+  text-align: right;
+}
+.mb-10 {
+}
+:deep(.uni-section-header) {
+  background-color: #fffaf6;
+}
+.sectioncontent {
+  width: 95%;
+  margin: auto;
+  border-radius: 6rpx;
+  margin: 20rpx auto;
+  box-sizing: border-box;
+  background-color: #fffaf6;
+}
+</style>
