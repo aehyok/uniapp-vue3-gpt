96387e981dc630caaf2d83638b45a83caf385e2b
刘启明 <455043818@qq.com>
2023-10-08 20:00:11 +0800

    feat:添加project过滤

--- src/pages/log/index.vue
+++ src/pages/log/index.vue
@@ -8,6 +8,15 @@
     ></uni-easyinput>
     <button style="display: flex; justify-content: center; align-items: center" @click="searchClick">查询</button>
   </view>
+  <view style="display: flex; justify-content: center; align-items: center; gap: 20px">
+    <view
+      v-for="(item, index) in ['console', 'mp', 'mini']"
+      :key="item"
+      @click="projectClick(item, index)"
+      :class="[index == state.selected ? 'selected' : 'not-selected']"
+      >{{ item }}</view
+    ></view
+  >
   <view v-for="(item, index) in state.list" :key="index">
     <view class="item">版本号：{{ item.version }}</view>
     <view class="item">时间：{{ format(new Date(item.createTime), "yyyy-MM-dd HH:mm:ss") }}</view>
@@ -24,14 +33,28 @@ import { format } from "date-fns";
 const http = new Request();
 
 // http://101.35.211.235:3001/api/v1/log/getListByVersion?version=3.1.0.002
+
 const state = reactive({
   version: "3.1.0.007",
   list: [],
+  selected: 0,
+  project: "",
 });
+const projectClick = (project, index) => {
+  if (index >= 0) {
+    state.selected = index;
+  }
+  state.project = project;
+  searchClick();
+};
 
 const searchClick = () => {
+  let url = `/so/api/v1/log/getListByVersion?version=${state.version}`;
+  if (state.project) {
+    url = `${url}&project=${state.project}`;
+  }
   http
-    .get(`/so/api/v1/log/getListByVersion?version=${state.version}`)
+    .get(url)
     .then((res) => {
       state.list = res.data.data;
     })
@@ -42,11 +65,28 @@ const searchClick = () => {
 </script>
 <style>
 button {
-  background-color: #9da3fb;
+  background-color: #18bc37;
   height: 55rpx;
+  color: white;
 }
 
 .item {
   margin: 0 40rpx;
+}
+
+.selected {
+  color: white;
+  border: 1px solid white;
+  border-radius: 6px;
+  background-color: #18bc37;
+  padding: 0 12px;
+}
+
+.not-selected {
+  color: white;
+  border: 1px solid white;
+  background-color: #8f939c;
+  border-radius: 6px;
+  padding: 0 12px;
 }
 </style>
