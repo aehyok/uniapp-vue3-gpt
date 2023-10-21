f175069087f6d7cdbeb1ba53fab6d71559b89e8b
刘启明 <455043818@qq.com>
2023-10-21 18:13:11 +0800

    chore:10月21日

--- src/pages/log/index.vue
+++ src/pages/log/index.vue
@@ -2,13 +2,13 @@
   <view style="display: flex; justify-content: space-between; align-items: center; padding: 10px">
     <uni-easyinput
       type="text"
-      v-model="state.version"
+      v-model="version"
       placeholder="请输入当前版本号"
       style="margin-right: 5rpx"
     ></uni-easyinput>
     <button style="display: flex; justify-content: center; align-items: center" @click="searchClick">查询</button>
   </view>
-  <view style="display: flex; justify-content: center; align-items: center; gap: 20px">
+  <view style="display: flex; justify-content: center; align-items: center; gap: 20px; margin-bottom: 20px">
     <view
       v-for="(item, index) in ['console', 'mp', 'mini']"
       :key="item"
@@ -29,16 +29,17 @@
 <script setup>
 import Request from "luch-request";
 import { reactive } from "vue";
+import { useStorage } from "@vueuse/core";
 import { format } from "date-fns";
 const http = new Request();
 
 // http://101.35.211.235:3001/api/v1/log/getListByVersion?version=3.1.0.002
+const version = useStorage("version", "3.1.0.008");
 
 const state = reactive({
-  version: "3.1.0.007",
   list: [],
   selected: 0,
-  project: "",
+  project: "console",
 });
 const projectClick = (project, index) => {
   if (index >= 0) {
@@ -49,7 +50,7 @@ const projectClick = (project, index) => {
 };
 
 const searchClick = () => {
-  let url = `/so/api/v1/log/getListByVersion?version=${state.version}`;
+  let url = `/so/api/v1/log/getListByVersion?version=${version.value}`;
   if (state.project) {
     url = `${url}&project=${state.project}`;
   }
@@ -62,6 +63,8 @@ const searchClick = () => {
       console.log(err, "catch");
     });
 };
+
+searchClick();
 </script>
 <style>
 button {
@@ -79,7 +82,7 @@ button {
   border: 1px solid white;
   border-radius: 6px;
   background-color: #18bc37;
-  padding: 0 12px;
+  padding: 4px 12px;
 }
 
 .not-selected {
@@ -87,6 +90,6 @@ button {
   border: 1px solid white;
   background-color: #8f939c;
   border-radius: 6px;
-  padding: 0 12px;
+  padding: 4px 12px;
 }
 </style>
